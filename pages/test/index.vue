f175e2f7b9a909c7febd057f32f19ea40abd73fb
atsuhiro <ishikawaatsuhito1205@gmail.com>
2024-11-23 22:01:09 +0900

    テストページ追加

---
+++ pages/test/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+useHead({
+  title: 'テスト:テストページ',
+  meta: [
+    { name: 'fwefrfr', content: 'Nuxt 3' }
+  ]
+})
+
+</script>
+
+<template>
+    <h2 @click="navigateTo('/')">テストページ</h2>
+</template>
